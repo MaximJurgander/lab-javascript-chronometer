9677c75b3314eb302b920d3b77b5aa0f82d27a29
Maxim Jurgandere <maxim.jurgander@gmail.com>
2020-10-24 22:34:55 +0200

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,39 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
-  startClick(callback) {
-    // ... your code goes here
+ 
+  startClick() {
+    const timeoutId = setInterval(() => {
+      this.currentTime++;
+    }, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    let minutes = 0;
+    if (this.currentTime % 60 === 0) {
+      minutes++;
+    }
+    if (minutes <= 0) {
+      return `${this.currentTime}`;
+    }
+    if (minutes => 0)
+      return `${minutes}${this.currentTime}`;
   }
   stopClick() {
-    // ... your code goes here
+   clearInterval(this.timeoutId);
   }
   resetClick() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    return `0${this.getMinutes()}:0${this.getSeconds()}`;
   }
 }
+
